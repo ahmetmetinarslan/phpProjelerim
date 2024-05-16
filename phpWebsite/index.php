2a28c762686fc3a2453b0a2e3c4495fb11aa69b9
Ahmet Metin Arslan <94191552+ahmetmetinarslan@users.noreply.github.com>
2024-05-16 15:39:29 +0300

    phpWebsite
    phpWebsite

---
+++ phpWebsite/index.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html lang="tr">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Anasayfa</title>
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+  <link rel="stylesheet" href="./owl/owl.carousel.min.css" />
+  <link rel="stylesheet" href="./owl/owl.theme.default.min.css" />
+
+  <link rel="stylesheet" href="./css/style.css" />
+</head>
+
+<body>
+  <section id="menu">
+    <div id="logo">Logo</div>
+    <nav>
+      <a href=""><i class="fa-solid fa-house icon"></i> Anasayfa</a>
+      <a href=""><i class="fa-solid fa-info icon"></i> Hakkımızda</a>
+      <a href=""><i class="fa-solid fa-graduation-cap icon"></i> Eğitimler</a>
+      <a href=""><i class="fa-solid fa-people-group icon"></i> Ekip</a>
+      <a href=""><i class="fa-solid fa-map-pin icon"></i> İletişim</a>
+    </nav>
+  </section>
+
+  <section id="anasayfa">
+    <div id="black"></div>
+    <div id="icerik">
+      <h2>Başlık</h2>
+      <hr width="300" align="left" />
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum quam
+        corporis cum quod doloribus, sit officia itaque! Non labore commodi id
+        necessitatibus pariatur quas recusandae sapiente soluta, iure corporis
+        neque veritatis expedita eaque nihil optio ut fugit magni repellendus
+        harum fugiat exercitationem cumque distinctio odio? Deserunt dolorum
+        tenetur debitis aspernatur.
+      </p>
+    </div>
+  </section>
+
+  <section id="hakkimizda">
+    <h3>Hakkımızda</h3>
+    <div class="container">
+      <div id="sol">
+        <h5 id="h5sol">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        </h5>
+      </div>
+      <div id="sag">
+        <span>L</span>
+        <p id="psag">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum,
+          pariatur soluta iste labore excepturi cupiditate quod sed minima
+          corrupti eligendi omnis molestias maxime neque quis in officiis odio
+          provident? Commodi?
+        </p>
+      </div>
+      <img src="./img/about.jpg" alt="" class="img-fluid mt100" />
+      <p id="pson">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+        iusto quibusdam dignissimos odio explicabo eum distinctio molestiae
+        eaque praesentium. Reiciendis.
+      </p>
+    </div>
+  </section>
+
+  <section id="egitimler">
+    <div class="container">
+      <h3>Eğitimler</h3>
+      <div class="owl-carousel owl-theme">
+        <div class="card item" data-merge="1.5">
+          <img src="./img/r2.jpg" alt="" class="img-fluid" />
+          <h5 class="baslikcard">HTML5 VE CSS3 Eğitim</h5>
+          <p class="cardp">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptatem, quod!
+          </p>
+        </div>
+
+        <div class="card item" data-merge="1.5">
+          <img src="./img/r2.jpg" alt="" class="img-fluid" />
+          <h5 class="baslikcard">HTML5 VE CSS3 Eğitim</h5>
+          <p class="cardp">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptatem, quod!
+          </p>
+        </div>
+
+        <div class="card item" data-merge="1.5">
+          <img src="./img/r2.jpg" alt="" class="img-fluid" />
+          <h5 class="baslikcard">HTML5 VE CSS3 Eğitim</h5>
+          <p class="cardp">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptatem, quod!
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="ekip">
+    <div class="container">
+      <h3 id="ekiph3">Ekip</h3>
+      <div class="sutun-sol-sag">
+        <img src="./img/ekip3.jpg" alt="" class="img-fluid oval" />
+        <h4 class="ekipisim">Name Surname</h4>
+        <p class="ekipp">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
+          maxime!
+        </p>
+        <div class="ekip-icon">
+          <a href="#"><i class="fa-brands fa-facebook social"></i></a>
+          <a href="#"><i class="fa-brands fa-google social"></i></a>
+          <a href="#"><i class="fa-brands fa-instagram social"></i></a>
+        </div>
+      </div>
+
+      <div class="sutun">
+        <img src="./img/ekip3.jpg" alt="" class="img-fluid oval" />
+        <h4 class="ekipisim">Name Surname</h4>
+        <p class="ekipp">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
+          maxime!
+        </p>
+        <div class="ekip-icon">
+          <a href="#"><i class="fa-brands fa-facebook social"></i></a>
+          <a href="#"><i class="fa-brands fa-google social"></i></a>
+          <a href="#"><i class="fa-brands fa-instagram social"></i></a>
+        </div>
+      </div>
+
+      <div class="sutun-sol-sag">
+        <img src="./img/ekip3.jpg" alt="" class="img-fluid oval" />
+        <h4 class="ekipisim">Name Surname</h4>
+        <p class="ekipp">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
+          maxime!
+        </p>
+        <div class="ekip-icon">
+          <a href="#"><i class="fa-brands fa-facebook social"></i></a>
+          <a href="#"><i class="fa-brands fa-google social"></i></a>
+          <a href="#"><i class="fa-brands fa-instagram social"></i></a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="iletisim">
+    <div class="container">
+      <h3 id="h3iletisim">İletişim</h3>
+      <form action="index.php" method="post">
+        <div id="iletisimopak">
+          <div id="formgroup">
+            <div id="solform">
+              <input type="text" name="isim" placeholder="Ad Soyad" class="form-control" required />
+              <input type="text" name="tel" placeholder="Telefon Numarası" class="form-control" required />
+            </div>
+            <div id="sagform">
+              <input type="email" name="mail" placeholder="Email Adresiniz" class="form-control" required />
+              <input type="text" name="konu" placeholder="Konu Başlığı Giriniz." class="form-control" required />
+            </div>
+            <textarea name="mesaj" placeholder="Mesaj Giriniz." id="" cols="30" rows="10" required class="form-control"></textarea>
+            <input type="submit" value="Gönder" />
+          </div>
+          <div id="adres">
+            <h4 id="adresbaslik">Ares:</h4>
+            <p class="adresp">Örnek Mahallesi</p>
+            <p class="adresp">Örnek Caddasi</p>
+            <p class="adresp">Örnek Sokak</p>
+            <p class="adresp">0212 555 55 55</p>
+            <p class="adresp">Email: ornek@ornek.com</p>
+          </div>
+        </div>
+      </form>
+      <footer>
+        <div id="copyright">2020 | Tüm Hakları Saklıdır.</div>
+        <div id="socialfooter">
+          <a href="#"><i class="fa-brands fa-facebook social"></i></a>
+          <a href="#"><i class="fa-brands fa-google social"></i></a>
+          <a href="#"><i class="fa-brands fa-instagram social"></i></a>
+        </div>
+        <a href="#menu"><i class="fa-solid fa-caret-up" id="up"></i></a>
+      </footer>
+    </div>
+  </section>
+
+  <script src="https://code.jquery.com/jquery-3.7.1.slim.min.js" integrity="sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8=" crossorigin="anonymous"></script>
+  <script src="./owl/owl.carousel.min.js"></script>
+  <script src="./owl/script.js"></script>
+</body>
+
+</html>
+
+
+<?php
+
+include("baglanti.php");
+
+if (isset($_POST['isim'], $_POST['tel'], $_POST['mail'], $_POST['konu'], $_POST['mesaj'])) {
+
+  $adsoyad = $_POST['isim'];
+  $telefon = $_POST['tel'];
+  $email = $_POST['mail'];
+  $konu = $_POST['konu'];
+  $mesaj = $_POST['mesaj'];
+
+  $ekle = "INSERT INTO iletisim (adsoyad, telefon, email, konu, mesaj) VALUES ('$adsoyad', '$telefon', '$email', '$konu', '$mesaj')";
+
+  if ($baglan->query($ekle) === TRUE) {
+    echo "<script> alert('Mesajınız Başarı ile Gönderilmiştir.') </script>";
+  } else {
+    echo "<script> alert('Mesajınız Gönderilirken Bir Hata Oluştu.') </script>";
+  }
+}
+
+?>
